4700e093ab3ce0ab4242f6f86c1270d78a6f3e94
Moroo Akira <retrage01@gmail.com>
2016-06-05 22:04:45 +0900

    Add vmbus_probe

--- sys/dev/acpi/vmbus/vmbus.c
+++ sys/dev/acpi/vmbus/vmbus.c
@@ -4,8 +4,15 @@
 #include <sys/kernel.h>
 #include <sys/device.h>
 #include <sys/types.h>
+#include <sys/condvar.h>
 
 #include <dev/acpi/acpivar.h>
+
+#include <dev/acpi/vmbus/hyperv/include/hyperv_busdma.h>
+#include <dev/acpi/vmbus/hyperv/vmbus/hv_sema.h>
+#include <dev/acpi/vmbus/hyperv/vmbus/hv_vmbus_priv.h>
+
+#include <dev/acpi/vmbus/hyperv/vmbus/vmbus_var.h>
 
 #define _COMPONENT	ACPI_BUS_COMPONENT
 ACPI_MODULE_NAME	("vmbus")
@@ -13,8 +20,11 @@ ACPI_MODULE_NAME	("vmbus")
 static int  vmbus_match(device_t, cfdata_t, void *);
 static void vmbus_attach(device_t, device_t, void *);
 
-struct vmbus_softc {
-	device_t	sc_dev;
+struct vmbus_softc	*vmbus_sc;
+
+static const char *vmbus_ids[] = {
+	"VMBUS",
+	NULL
 };
 
 CFATTACH_DECL_NEW(vmbus, sizeof(struct vmbus_softc),
@@ -23,13 +33,15 @@ CFATTACH_DECL_NEW(vmbus, sizeof(struct vmbus_softc),
 static int
 vmbus_match(device_t parent, cfdata_t match, void *aux)
 {
-	struct acpi_attach_args *aaa = aux;
-	struct acpi_devnode *node = aaa->aa_node;
+	struct acpi_attach_args *aa = aux;
 
-	if (strcmp(node->ad_name, "VMB8") != 0)
+	if (aa->aa_node->ad_type != ACPI_TYPE_DEVICE)
 		return 0;
 
-	return 1;
+	if (acpi_match_hid(aa->aa_node->ad_devinfo, vmbus_ids))
+		return 1;
+
+	return 0;
 }
 
 static void
